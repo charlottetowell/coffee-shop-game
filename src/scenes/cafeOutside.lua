5dcedf96b4d3c8e6c6079462cab0d31d2418c85a
Charlotte Towell <charlotte.m.towell@gmail.com>
2025-07-29 20:47:35 +1000

    set up cloud animations

--- src/scenes/cafeOutside.lua
+++ src/scenes/cafeOutside.lua
@@ -6,17 +6,27 @@ function cafeOutside:draw()
     -- set background colour
     love.graphics.setBackgroundColor(colours.sky.r, colours.sky.g, colours.sky.b)
 
-    -- draw cafe
-    love.graphics.draw(assets.cafeOutside, 0, 0, 0, pixelScale, pixelScale)
-
     -- draw ground
     love.graphics.setColor(colours.ground.r, colours.ground.g, colours.ground.b)
     love.graphics.rectangle("fill", 0, windowHeight-(4*pixelScale), windowWidth, 4*pixelScale)
 
-    -- draw clouds
+    -- draw clouds (animated scrolling)
     setColourWhite()
-    love.graphics.draw(assets.clouds.image, assets.clouds.quads[1], windowWidth * 0.1, windowHeight * 0.1, 0, pixelScale, pixelScale)
-    love.graphics.draw(assets.clouds.image, assets.clouds.quads[2], windowWidth * 0.6, windowHeight * 0.25, 0, pixelScale, pixelScale)
-    love.graphics.draw(assets.clouds.image, assets.clouds.quads[3], windowWidth * 0.2, windowHeight * 0.4, 0, pixelScale, pixelScale)
-    love.graphics.draw(assets.clouds.image, assets.clouds.quads[4], windowWidth * 0.7, windowHeight * 0.55, 0, pixelScale, pixelScale)
+    local time = love.timer.getTime()
+    local cloudSpeeds = {20, 15, 25, 12}
+    local cloudStartX = {windowWidth * 0.9, windowWidth * 0.6, windowWidth * 0.2, windowWidth * 0.7}
+    local cloudYPositions = {windowHeight * 0.1, windowHeight * 0.3, windowHeight * 0.5, windowHeight * 0.75}
+    
+    local cloudsPerRow = 2
+    for cloudSet = 0, (cloudsPerRow-1) do -- number of cloud sets (per row)
+        local offset = cloudSet * (windowWidth + 100) / cloudsPerRow
+        
+        for i = 1, 4 do -- draw clouds for each row
+            local cloudX = (cloudStartX[i] - time * cloudSpeeds[i] + offset) % (windowWidth + 100) - 100
+            love.graphics.draw(assets.clouds.image, assets.clouds.quads[i], cloudX, cloudYPositions[i], 0, pixelScale, pixelScale)
+        end
+    end
+
+    -- draw cafe
+    love.graphics.draw(assets.cafeOutside, 0, 0, 0, pixelScale, pixelScale)
 end
